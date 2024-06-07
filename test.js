3525102fb7dedf9025dcf4b28a26529f18801948
Hector Flores <106941903+hekmaflo@users.noreply.github.com>
2024-06-07 15:56:04 -0500

    added test script for practive

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-for (let i = 0; i < 3; i++) {
-  console.log(i);
+let colors = ["Red", "Greem", "Blue"];
+for (let [index, item] of colors.entries()) {
+  console.log(`${index}: ${item} is a color`);
+}
+
+let letters = "Hector";
+
+for (let letter of letters) {
+  console.log(letter);
 }
